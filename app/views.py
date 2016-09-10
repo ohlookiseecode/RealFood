080d6d790b343d6e580f89d8e34676f1a1d2b944
Hannah <its.mahon@gmail.com>
2016-09-10 09:24:58 -0400

    added some stuff

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
-from flask import render_template
+# PennApps XIV
+
+# imports
+from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
 from app import app
+import sqlite3 as lite
+import sys
+import cgi
+import resetDB
+import socket
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +17,15 @@ def index():
     return render_template('index.html',
                            title='Home',
                            user=user)
+
+@app.route('/signuppageres')
+def signup1():
+    return render_template('CREATEPAGE.html', user = current_user, title='Signup/Login')
+
+@app.route('/signuppatefoo')
+def signup2():
+    return render_template('CREATEPAGE.html', user = current_user, title='Signup/Login')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', user = current_user, title='About')
